b5958473a09ffa0957165674fe7b4ebb96b8a79c
aajihoon12 <aajihoon12@gmail.com>
2020-09-21 13:16:23 +0900

    CH03

---
+++ CH03_PointListMain.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "CH03_ArrayList.h"
+#include "CH03_Point.h"
+
+main() {
+    List list;
+    Point comPos;
+    Point * ppos;
+
+    ListInit(&list);
+
+    ppos = (Point*)malloc(sizeof(Point));
+    SetPointPos(ppos, 2, 1);
+    LInsert(&list, ppos);
+
+    ppos = (Point*)malloc(sizeof(Point));
+    SetPointPos(ppos, 2, 2);
+    LInsert(&list, ppos);
+
+    ppos = (Point*)malloc(sizeof(Point));
+    SetPointPos(ppos, 3, 1);
+    LInsert(&list, ppos);
+
+    ppos = (Point*)malloc(sizeof(Point));
+    SetPointPos(ppos, 3, 2);
+    LInsert(&list, ppos);
+
+    printf("Number of current data: %d \n", LCount(&list));
+
+    if(LFirst(&list, &ppos)) {
+        ShowPointPos(ppos);
+
+        while(LNext(&list, &ppos)) {
+            ShowPointPos(ppos);
+        }
+    }
+    printf("\n");
+
+    comPos.xpos=2;
+    comPos.ypos=0;
+
+    if(LFirst(&list, &ppos)) {
+        if(PointComp(ppos, &comPos)==1) {
+            ppos = LRemove(&list);
+            free(ppos);
+        }
+
+        while(LNext(&list, &ppos)) {
+            if(PointComp(ppos, &comPos)==1) {
+                ppos = LRemove(&list);
+                free(ppos);
+            }
+        }
+    }
+    printf("Number of current data: %d \n", LCount(&list));
+
+    if(LFirst(&list, &ppos)) {
+        ShowPointPos(ppos);
+
+        while(LNext(&list, &ppos)) {
+            ShowPointPos(ppos);
+        }
+        printf("\n");
+
+        return 0;
+    }
+}

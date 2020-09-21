934fb9d1ee53e1db205a05c7b83fc23d1ae3d2e3
aajihoon12 <aajihoon12@gmail.com>
2020-09-21 12:55:08 +0900

    CH03

---
+++ CH03_Point.h
@@ -0,0 +1,15 @@
+#ifndef __POINT_H__
+#define __POINT_H__
+
+typedef struct _point {
+    int xpos;
+    int ypos;
+}Point;
+
+void SetPointPos(Point * pos, int xpos, int ypos);
+
+void ShowPointPos(Point * ppos);
+
+int PointComp(Point * ppos1, Point * ppos2);
+
+#endif
